11a2e74f2518f385beeabcb153a440f499c94a8b
Yanet Cesaire Velazquez <81645674+86yanetc@users.noreply.github.com>
2024-08-25 00:39:50 -0300

    Add files via upload

---
+++ Caso_Estudio_11.sql
@@ -0,0 +1,14 @@
+DROP TABLE src_dest_distance;
+CREATE TABLE src_dest_distance
+(
+    source          varchar(20),
+    destination     varchar(20),
+    distance        int
+);
+INSERT INTO src_dest_distance VALUES ('Bangalore', 'Hyderbad', 400);
+INSERT INTO src_dest_distance VALUES ('Hyderbad', 'Bangalore', 400);
+INSERT INTO src_dest_distance VALUES ('Mumbai', 'Delhi', 400);
+INSERT INTO src_dest_distance VALUES ('Delhi', 'Mumbai', 400);
+INSERT INTO src_dest_distance VALUES ('Chennai', 'Pune', 400);
+INSERT INTO src_dest_distance VALUES ('Pune', 'Chennai', 400);
+
