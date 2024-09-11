3818cf630416d3e4e6925643b31b5d68b9276d36
Yanet Cesaire Velazquez <81645674+86yanetc@users.noreply.github.com>
2024-09-11 20:17:23 -0300

    Add files via upload

---
+++ Proyecto16.sql
@@ -0,0 +1,129 @@
+
+CREATE TABLE products 
+(
+  productline character varying(50),
+  productvendor character varying(50),
+  quantityinstock integer
+);
+
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Motorcycles','Min Lin Diecast',7933),
+	 ('Classic Cars','Classic Metal Creations',7305),
+	 ('Motorcycles','Highway 66 Mini Classics',6625),
+	 ('Motorcycles','Red Start Diecast',5582),
+	 ('Classic Cars','Motor City Art Classics',3252),
+	 ('Classic Cars','Second Gear Diecast',6791),
+	 ('Classic Cars','Autoart Studio Design',68),
+	 ('Classic Cars','Second Gear Diecast',3619),
+	 ('Trucks and Buses','Welly Diecast Productions',1579),
+	 ('Motorcycles','Unimax Art Galleries',9997);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Classic Cars','Welly Diecast Productions',6906),
+	 ('Classic Cars','Welly Diecast Productions',9123),
+	 ('Classic Cars','Second Gear Diecast',1049),
+	 ('Classic Cars','Studio M Art Models',5663),
+	 ('Trucks and Buses','Exoto Designs',6125),
+	 ('Classic Cars','Welly Diecast Productions',7323),
+	 ('Trucks and Buses','Studio M Art Models',2613),
+	 ('Classic Cars','Highway 66 Mini Classics',3975),
+	 ('Vintage Cars','Motor City Art Classics',8693),
+	 ('Vintage Cars','Studio M Art Models',8635);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Classic Cars','Classic Metal Creations',9042),
+	 ('Planes','Red Start Diecast',5330),
+	 ('Vintage Cars','Welly Diecast Productions',2724),
+	 ('Classic Cars','Gearbox Collectibles',8826),
+	 ('Classic Cars','Min Lin Diecast',9772),
+	 ('Classic Cars','Gearbox Collectibles',4724),
+	 ('Vintage Cars','Motor City Art Classics',540),
+	 ('Trucks and Buses','Unimax Art Galleries',8258),
+	 ('Vintage Cars','Autoart Studio Design',9354),
+	 ('Trucks and Buses','Carousel DieCast Legends',2018);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Planes','Gearbox Collectibles',992),
+	 ('Motorcycles','Welly Diecast Productions',4357),
+	 ('Vintage Cars','Gearbox Collectibles',548),
+	 ('Classic Cars','Red Start Diecast',8164),
+	 ('Vintage Cars','Carousel DieCast Legends',4189),
+	 ('Vintage Cars','Min Lin Diecast',5649),
+	 ('Ships','Min Lin Diecast',4259),
+	 ('Vintage Cars','Red Start Diecast',5992),
+	 ('Vintage Cars','Unimax Art Galleries',3913),
+	 ('Classic Cars','Unimax Art Galleries',8347);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Classic Cars','Highway 66 Mini Classics',7733),
+	 ('Trains','Carousel DieCast Legends',6450),
+	 ('Classic Cars','Min Lin Diecast',1917),
+	 ('Vintage Cars','Exoto Designs',7913),
+	 ('Classic Cars','Gearbox Collectibles',9127),
+	 ('Classic Cars','Gearbox Collectibles',8990),
+	 ('Motorcycles','Studio M Art Models',7689),
+	 ('Vintage Cars','Exoto Designs',2378),
+	 ('Classic Cars','Min Lin Diecast',5545),
+	 ('Vintage Cars','Exoto Designs',6553);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Vintage Cars','Exoto Designs',8290),
+	 ('Trucks and Buses','Motor City Art Classics',3128),
+	 ('Ships','Carousel DieCast Legends',1956),
+	 ('Vintage Cars','Studio M Art Models',6645),
+	 ('Classic Cars','Classic Metal Creations',1249),
+	 ('Classic Cars','Studio M Art Models',3209),
+	 ('Classic Cars','Unimax Art Galleries',1005),
+	 ('Classic Cars','Highway 66 Mini Classics',4074),
+	 ('Motorcycles','Autoart Studio Design',7003),
+	 ('Classic Cars','Carousel DieCast Legends',8197);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Planes','Classic Metal Creations',3627),
+	 ('Vintage Cars','Motor City Art Classics',7332),
+	 ('Motorcycles','Highway 66 Mini Classics',15),
+	 ('Ships','Carousel DieCast Legends',1898),
+	 ('Vintage Cars','Classic Metal Creations',2847),
+	 ('Trucks and Buses','Autoart Studio Design',2327),
+	 ('Motorcycles','Highway 66 Mini Classics',6840),
+	 ('Classic Cars','Classic Metal Creations',2350),
+	 ('Classic Cars','Carousel DieCast Legends',2542),
+	 ('Planes','Autoart Studio Design',5942);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Classic Cars','Exoto Designs',1452),
+	 ('Classic Cars','Second Gear Diecast',7723),
+	 ('Vintage Cars','Min Lin Diecast',9173),
+	 ('Classic Cars','Exoto Designs',4695),
+	 ('Classic Cars','Welly Diecast Productions',7995),
+	 ('Vintage Cars','Autoart Studio Design',2902),
+	 ('Classic Cars','Gearbox Collectibles',9446),
+	 ('Vintage Cars','Carousel DieCast Legends',6621),
+	 ('Classic Cars','Classic Metal Creations',6600),
+	 ('Planes','Second Gear Diecast',6812);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Vintage Cars','Red Start Diecast',2081),
+	 ('Classic Cars','Exoto Designs',6582),
+	 ('Vintage Cars','Second Gear Diecast',4710),
+	 ('Planes','Unimax Art Galleries',2756),
+	 ('Classic Cars','Classic Metal Creations',7869),
+	 ('Trucks and Buses','Motor City Art Classics',5099),
+	 ('Motorcycles','Exoto Designs',178),
+	 ('Motorcycles','Gearbox Collectibles',9241),
+	 ('Trucks and Buses','Classic Metal Creations',2874),
+	 ('Trains','Gearbox Collectibles',8601);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Trucks and Buses','Red Start Diecast',814),
+	 ('Vintage Cars','Highway 66 Mini Classics',136),
+	 ('Motorcycles','Second Gear Diecast',3341),
+	 ('Vintage Cars','Studio M Art Models',7062),
+	 ('Trucks and Buses','Highway 66 Mini Classics',1016),
+	 ('Trains','Classic Metal Creations',1645),
+	 ('Motorcycles','Autoart Studio Design',600),
+	 ('Ships','Autoart Studio Design',1897),
+	 ('Planes','Min Lin Diecast',5841),
+	 ('Ships','Studio M Art Models',737);
+INSERT INTO products (productline,productvendor,quantityinstock) VALUES
+	 ('Ships','Unimax Art Galleries',3501),
+	 ('Planes','Motor City Art Classics',9653),
+	 ('Ships','Red Start Diecast',7083),
+	 ('Classic Cars','Carousel DieCast Legends',6934),
+	 ('Planes','Highway 66 Mini Classics',7106),
+	 ('Planes','Motor City Art Classics',551),
+	 ('Ships','Welly Diecast Productions',5088),
+	 ('Planes','Second Gear Diecast',8820),
+	 ('Planes','Motor City Art Classics',4857),
+	 ('Ships','Unimax Art Galleries',414);
