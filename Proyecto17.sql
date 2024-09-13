4a29a1765ccac5c61ab395032ad7460c2c895a31
Yanet Cesaire Velazquez <81645674+86yanetc@users.noreply.github.com>
2024-09-13 09:07:09 -0300

    Proyecto17.sql

--- Proyecto17.sql
+++ Proyecto17.sql
@@ -1,7 +1,7 @@
 drop table if exists customer;
 create table customer
 (
-	id		int primary key,
+	id	int primary key,
 	name	varchar(30),
 	email	varchar(50)
 );
@@ -10,31 +10,31 @@ create table customer
 drop table if exists bike;
 create table bike
 (
-	id					int primary key,
-	model				varchar(50),
-	category			varchar(50),
+	id			int primary key,
+	model			varchar(50),
+	category		varchar(50),
 	price_per_hour		decimal,
 	price_per_day		decimal,
-	status				varchar(20)
+	status			varchar(20)
 );
 
 
 drop table if exists rental;
 create table rental
 (
-	id					int primary key,
-	customer_id			int references customer(id),
-	bike_id				int references bike(id),
+	id			int primary key,
+	customer_id		int references customer(id),
+	bike_id			int references bike(id),
 	start_timestamp		timestamp,
-	duration			int,
-	total_paid			decimal
+	duration		int,
+	total_paid		decimal
 );
 
 
 drop table if exists membership_type;
 create table membership_type
 (
-	id				int primary key,
+	id			int primary key,
 	name			varchar(50),
 	description		varchar(500),
 	price			decimal
@@ -44,12 +44,12 @@ create table membership_type
 drop table if exists membership;
 create table membership
 (
-	id					int primary key,
+	id			int primary key,
 	membership_type_id	int references membership_type(id),
-	customer_id			int references customer(id),
-	start_date			date,
-	end_date			date,
-	total_paid			decimal
+	customer_id		int references customer(id),
+	start_date		date,
+	end_date		date,
+	total_paid		decimal
 );
 
 
