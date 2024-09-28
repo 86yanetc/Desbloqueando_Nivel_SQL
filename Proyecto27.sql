f408e138f2ee12dda7117ce9824f56b69e819885
Yanet Cesaire Velazquez <81645674+86yanetc@users.noreply.github.com>
2024-09-28 11:59:11 -0300

    Add files via upload

---
+++ Proyecto27.sql
@@ -0,0 +1,16 @@
+drop table if exists emp_input;
+create table emp_input
+(
+id      int,
+name    varchar(40)
+);
+insert into emp_input values (1, 'Emp1');
+insert into emp_input values (2, 'Emp2');
+insert into emp_input values (3, 'Emp3');
+insert into emp_input values (4, 'Emp4');
+insert into emp_input values (5, 'Emp5');
+insert into emp_input values (6, 'Emp6');
+insert into emp_input values (7, 'Emp7');
+insert into emp_input values (8, 'Emp8');
+
+
