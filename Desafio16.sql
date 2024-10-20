303c8c62b84903634c2d8f7c91469b440e14a708
Yanet Cesaire Velazquez <81645674+86yanetc@users.noreply.github.com>
2024-10-20 12:45:19 -0300

    Add files via upload

---
+++ Desafio16.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS covid_cases;
+create TABLE covid_cases
+(
+	cases_reported	int,
+	dates			date	
+);
+
+INSERT INTO covid_cases VALUES(20124,to_date('10/01/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(40133,to_date('15/01/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(65005,to_date('20/01/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(30005,to_date('08/02/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(35015,to_date('19/02/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(15015,to_date('03/03/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(35035,to_date('10/03/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(49099,to_date('14/03/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(84045,to_date('20/03/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(100106,to_date('31/03/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(17015,to_date('04/04/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(36035,to_date('11/04/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(50099,to_date('13/04/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(87045,to_date('22/04/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(101101,to_date('30/04/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(40015,to_date('01/05/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(54035,to_date('09/05/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(71099,to_date('14/05/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(82045,to_date('21/05/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(90103,to_date('25/05/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(99103,to_date('31/05/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(11015,to_date('03/06/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(28035,to_date('10/06/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(38099,to_date('14/06/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(45045,to_date('20/06/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(36033,to_date('09/07/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(40011,to_date('23/07/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(25001,to_date('12/08/2020','DD/MM/YYYY'));
+INSERT INTO covid_cases VALUES(29990,to_date('26/08/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(20112,to_date('04/09/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(43991,to_date('18/09/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(51002,to_date('29/09/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(26587,to_date('25/10/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(11000,to_date('07/11/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(35002,to_date('16/11/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(56010,to_date('28/11/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(15099,to_date('02/12/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(38042,to_date('11/12/2020','DD/MM/YYYY'));	
+INSERT INTO covid_cases VALUES(73030,to_date('26/12/2020','DD/MM/YYYY'));	
